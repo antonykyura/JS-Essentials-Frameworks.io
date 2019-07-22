11c7f13bdb511a9fd82389a74c65ac940ed1845b
antonykyura <antonyk3663@gmail.com>
2019-07-22 21:21:34 +0200

    change task4

--- main.js
+++ main.js
@@ -18,11 +18,15 @@ sumAll(4); // 10
 
 
 // task4
-    function factorial(n) {
-        return n ? n * factorial(n - 1) : 1;
-      }
-factorial(3); // 6
-factorial(5); // 120
+function factorial(n) {
+  if (n<0){
+    return ('error');
+  }
+  return n ? n * factorial(n - 1) : 1;
+}
+console.log(factorial(3)); // 6
+console.log(factorial(5)); // 120
+console.log(factorial(-2));
 
 //task6
 function filterNumbers(arr, maxNumber) {  
